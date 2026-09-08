98e0483a76ccc78a96e35e8be2e000541fdee24a
Lasse Egebo <lae003@edu.zealand.dk>
2026-09-08 17:13:41 +0200

    Finished user story 2 in which I added a button to the index page which sorts the job postings by ascending or descending date.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,13 +13,28 @@ public class IndexModel : PageModel
     public IndexModel(GraduateJobFinderContext context)
     {
         _context = context;
-        
     }
+
+    public string NextDateSort { get; set; }
 
     public IList<JobPosting> JobPostings { get; set; } = default!;
 
-    public async Task OnGetAsync()
+    public async Task OnGetAsync(string sortOrder)
     {
-        JobPostings = await _context.JobPosting.ToListAsync();
+        NextDateSort = sortOrder == "date_asc" ? "date_desc" : "date_asc";
+
+        IQueryable<JobPosting> jobPostsIQ = _context.JobPosting;
+
+        switch (sortOrder)
+        {
+            case "date_asc":
+                jobPostsIQ = jobPostsIQ.OrderBy(j => j.Date);
+                break;
+            default:
+                jobPostsIQ = jobPostsIQ.OrderByDescending(j => j.Date);
+                break;
+        }
+
+        JobPostings = await jobPostsIQ.AsNoTracking().ToListAsync();
     }
 }
--- obj/Debug/net10.0/GraduateJobFinder.AssemblyInfo.cs
+++ obj/Debug/net10.0/GraduateJobFinder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraduateJobFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2248d82efe3a39e3a900b0548603b61c82463f84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5491c2e6a576bf283775ddef58c75e093ad038")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraduateJobFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraduateJobFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
